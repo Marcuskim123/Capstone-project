9d6ab8856b6ce44626b55011f4d9242413012b54
KnockOut <107820928+Marcuskim123@users.noreply.github.com>
2025-11-26 13:14:25 +0900

    improved signup page

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -11,31 +11,49 @@ import {
 import { Input } from "@/components/ui/input";
 import {Checkbox} from "@/components/ui/checkbox";
 import {useState} from "react";
+import { Button } from "./ui/button";
 
 
 export default function Signup() {
-    const [Username, setUsername] = useState("");
-    const [Email, setEmail] = useState("");
-    const [Password, setPassword] = useState("");
+
     const [confirmPassword, setConfirmPassword] = useState("");
     const [ToS, setToS] = useState(false);
-    const [BoD, setBoD] = useState("");
+    const [userInfo, setUserInfo] = useState({
+        email:"",
+        username:"",
+        password:"",
+    });
+    const [checkUsername, setCheckUsername] = useState("");
 
 
-    const sendSignUp = () => {
-        console.log("Signup!");
+    const sendSignUp = async (e) => {
+        e.preventDefault();
+        const handleForm = new FormData(e.target);
+        setEmail(handleForm.get('email'));
+        setPassword(handleForm.get('password'));
+
         try {
-
+            console.log(Tos);
         }
-        catch(e) {
-            console.log(e); 
+        catch(err) {
+            console.log(err);
         }
     };
+
+    const checkPassword = async (event) => {
+        if (event.target.value == userInfo.password) {
+            setConfirmPassword("Password is correct");
+        }
+        else{
+            setConfirmPassword("Password is does not match")
+        }
+        
+    }
 
 
   return (
     <div className="w-full grid">
-      <form>
+      <form onSubmit={sendSignUp}>
         <FieldGroup className="flex w-full justified-center text-left">
           <FieldSet>
             <FieldLegend>Account Signup</FieldLegend>
@@ -43,18 +61,19 @@ export default function Signup() {
             <FieldGroup className="max-w-lg">
               <Field>
                 <FieldLabel>Email</FieldLabel>
-                <Input type="email" placeholder="Example@email.com" required/>
+                <Input name="email" type="email" placeholder="Example@email.com" required />
               </Field>
               <Field>
                 <FieldLabel>Username</FieldLabel>
-                <Input type="text" placeholder="ex. KnockKnock11" required/>
+                <Input type="text" placeholder="ex. KnockKnock11" required />
+                <FieldDescription>{checkUsername}</FieldDescription>
               </Field>
               <Field>
                 <FieldLabel>Password</FieldLabel>
-                <Input type="password" placeholder="•••••••" required/>
+                <Input name="password" type="password" placeholder="•••••••" required/>
                 <FieldDescription>Password must be atleast 8 letters long</FieldDescription>
                 <FieldLabel>Confirm Password</FieldLabel>
-                <Input type="password" placeholder="•••••••" required/>
+                <Input type="password" placeholder="•••••••" required onChange={checkPassword}/>
                 <FieldDescription>{}</FieldDescription>
               </Field>
             </FieldGroup>
@@ -62,8 +81,11 @@ export default function Signup() {
             <FieldGroup className="flex flex-col gap-6">
                 <FieldLabel>Terms of Service</FieldLabel>
                 <Field orientation="horizontal" className="flex items-center gap-3">
-                    <Checkbox required id="terms"/>
-                    <FieldLabel>I agree to terms of service and follow while use of the service</FieldLabel>
+                    <Checkbox required id="terms" onChange={(e) => setToS(e.target.value)}/>
+                    <FieldLabel>I agree to terms of service</FieldLabel>
+                </Field>
+                <Field>
+                    <Button type="submit">Sumbit</Button>
                 </Field>
             </FieldGroup>
           </FieldSet>
--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -8,31 +8,35 @@ import {
   NavigationMenuTrigger,
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Topbar() {
-  
+
   return (
     <div className="w-full flex justify-center">
       <NavigationMenu className="w-full max-w-lg">
-        <NavigationMenuList className="flex flex-row gap-10 p-4 font-large justify-center">
-          <NavigationMenuItem>
+        <NavigationMenuList>
+          <div className="flex flex-row gap-10 p-4 font-large justify-center">
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <Link href="/">Home</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <Link href="/dashboard">Dashboard</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <Link href="/dashboard">Contact</Link>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </div>
+          <NavigationMenuItem className="">
             <NavigationMenuLink asChild>
-              <Link href="/">Home</Link>
+              <Link href="/signup">Sign up</Link>
             </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink asChild>
-              <Link href="/dashboard">Dashboard</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink asChild>
-              <Link href="/dashboard">Contact</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink>Sign up</NavigationMenuLink>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
