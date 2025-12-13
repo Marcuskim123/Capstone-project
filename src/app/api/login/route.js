78d03130d564cc547b7cc2eeb9da13f02526a661
KnockOut <wooreehyun@gmail.com>
2025-12-13 15:04:13 +0900

    bake cookies

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,7 +1,7 @@
 import { AuthCredential, signInWithEmailAndPassword } from "firebase/auth";
-import {auth} from "@/lib/firebase.config"
-import { redirect } from "next/navigation";
-import { revalidatePath } from "next/cache";
+import { auth } from "@/lib/firebase.config"
+import { cookies } from "next/headers";
+
 
 
 export async function POST(request) {
@@ -10,25 +10,33 @@ export async function POST(request) {
         // const req = await request.FormData();
         // const email = req.get("email");
         // const password = req.get("password");
-        const userCredential = await signInWithEmailAndPassword(auth,email,password);
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
         const signedUser = userCredential.user.uid;
         const token = await userCredential.user.getIdToken();
-        const refToken = userCredential.user.refreshToken;
+        const refToken = userCredential.user.refreshToken;  
 
-        return Response.json({
-            message:`You have been login to ${signedUser}`,
-            user:signedUser,
-            email:userCredential.user.email,
-            token:token,
-            refreshToken:refToken,
-        }, {status:200});
+        return new Response(
+            JSON.stringify({
+                message: `You have been login to ${signedUser}`,
+                user: signedUser,
+                email: userCredential.user.email,
 
-        
+            }),
+            {
+                status: 200,
+                headers: {
+                    "Content-Type":"application/json",
+                    "Set-Cookie":`IdToken=${token}`
+                }
+
+            });
+
+
 
     }
-    catch(e){
+    catch (e) {
         console.log("API ERROR " + e);
-        return new Response("There is error at authenticating please try again", {status:500});
+        return new Response("There is error at authenticating please try again", { status: 500 });
     }
 
 }
